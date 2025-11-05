68d7750bcbcf2bb9e4c8f36f4693d1ef4562ceec
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-11-05 14:47:50 +0100

    refactor: Move permission action display to the correct position in ProductEdit component

--- resources/views/livewire/product-edit.blade.php
+++ resources/views/livewire/product-edit.blade.php
@@ -1,9 +1,10 @@
 <div class="min-h-screen bg-gray-50 py-8 dark:bg-gray-900">
 
     <div class="mx-auto max-w-7xl space-y-6 px-4 sm:px-6 lg:px-8">
-        {{ $this->permissionAction }}
+
         <!-- Product Information Form -->
         <div class="overflow-hidden rounded-lg bg-white shadow dark:bg-gray-800">
+            {{ $this->permissionAction }}
             <form wire:submit="updateProduct">
                 {{ $this->productForm }}
 
