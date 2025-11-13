f6dd0ecd83ebc7cd1e5e6f648eb247a9a8fb0475
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-11-13 13:51:10 +0100

    refactor: Increase mobile menu icon sizes for improved accessibility

--- resources/views/components/layouts/navigation.blade.php
+++ resources/views/components/layouts/navigation.blade.php
@@ -102,13 +102,13 @@
                     <span class="absolute -inset-0.5"></span>
                     <span class="sr-only">{{ __('Open main menu') }}</span>
                     <!-- Menu open: "hidden", Menu closed: "block" -->
-                    <svg class="block w-8 h-8" aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    <svg class="block w-12 h-12" aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
                     <!-- Menu open: "block", Menu closed: "hidden" -->
-                    <svg class="hidden w-8 h-8" aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    <svg class="hidden w-12 h-12" aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                     </svg>
